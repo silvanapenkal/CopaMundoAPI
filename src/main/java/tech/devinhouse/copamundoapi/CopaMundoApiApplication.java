d3e8687fb20890fe0e5c384e1268aa976510bf46
silvanapenkal <silvanapenkal@gmail.com>
2022-11-15 18:31:43 -0300

    Exercício semana 10 do Devinhouse

---
+++ src/main/java/tech/devinhouse/copamundoapi/CopaMundoApiApplication.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.copamundoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopaMundoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CopaMundoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/devinhouse/copamundoapi/CopaMundoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.copamundoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopaMundoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
